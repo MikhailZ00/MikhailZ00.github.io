efea28d3eda7e4e148e02cc02aec4fae05647e63
MikhailZ00 <55312268+MikhailZ00@users.noreply.github.com>
2020-05-23 20:33:23 +0300

    homework

---
+++ homework/precache-manifest.abae9e45e792bc6dcde184044a8003b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe4d1ed88f2aad91b07ede7afb70bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8643340ef782ccd085ef",
+    "url": "/static/css/main.62887aae.chunk.css"
+  },
+  {
+    "revision": "52d1d84623908097307b",
+    "url": "/static/js/2.839522f0.chunk.js"
+  },
+  {
+    "revision": "8643340ef782ccd085ef",
+    "url": "/static/js/main.84b69aaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
