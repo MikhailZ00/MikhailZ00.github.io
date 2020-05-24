7535ef078026c82789f45ecde844f32bc69d2592
MikhailZ00 <55312268+MikhailZ00@users.noreply.github.com>
2020-05-24 13:27:33 +0300

    Add files via upload

---
+++ lab5/precache-manifest.507b5997901da6dff09165686e235af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f17812172429f5e37ff388e3498000c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9579708e2602124539b9",
+    "url": "/static/css/main.62887aae.chunk.css"
+  },
+  {
+    "revision": "52d1d84623908097307b",
+    "url": "/static/js/2.839522f0.chunk.js"
+  },
+  {
+    "revision": "9579708e2602124539b9",
+    "url": "/static/js/main.92e6cac0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
